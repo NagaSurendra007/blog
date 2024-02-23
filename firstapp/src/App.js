a19f3d4362e89a9adcdf054ab8d27d65ec70a2ea
mangesh <leelamangeshmunagala@gmail.com>
2024-02-23 17:38:39 +0530

    created NavBar

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,5 +1,5 @@
-
-  
+import NavBar from "./pages/NavBar"
+import './App.css'  
 import {BrowserRouter,Routes,Route } from "react-router-dom"
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
@@ -12,16 +12,17 @@ function App(){
      <BrowserRouter>
         <div className = "App">
             <h1>First React Page</h1>
+            <NavBar></NavBar>
             <div id ="page-body">           
         <Routes>
               <Route path ="/" element = {<HomePage/>}></Route>
              <Route path ="/About" element = {<AboutPage/>}></Route>
-             <Route path ="/Article" element = {<ArticlePage/>}></Route>
-             <Route path ="/Articles/:articleId" element = {<ArticlesListPage/>}></Route>
+             <Route path ="/Articles/:articleId" element = {<ArticlePage/>}></Route>
+             <Route path ="/Articles" element = {<ArticlesListPage/>}></Route>
              <Route path ="*" element = {<NotfoundPage/>}></Route>
            
         </Routes>
-        </div>
+            </div>
         </div>
     </BrowserRouter>
 
