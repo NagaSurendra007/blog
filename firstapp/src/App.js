4fa8ec37c31a5b2455d58f8480a09fd3d4a817b6
mangesh <leelamangeshmunagala@gmail.com>
2024-02-26 14:59:36 +0530

    created dummy database Article-content.js

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,4 +1,4 @@
-import NavBar from "./pages/NavBar"
+import NavBar from "./NavBar"
 import './App.css'  
 import {BrowserRouter,Routes,Route } from "react-router-dom"
 import AboutPage from "./pages/AboutPage";
--- firstapp/src/pages/AboutPage.js
+++ firstapp/src/pages/AboutPage.js
@@ -1,7 +1,7 @@
 function AboutPage(){
     return(
         <>
-       AboutPage
+       <h1>Continental food refers to dishes made and consumed in the European countries. Dishes of French, Spanish and Italian cuisine fall under the category of ‘Continental food’. The key specialty of this food is, they concentrate more on ingredients like olive oil, wine, herbs and minimal spices. Akshay adds, "Continental food involves cooking techniques like frying, roasting and baking. Also, the food is rich and usually high in calories."</h1>
         </>
     );
 }
